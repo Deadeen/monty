7b9904af311a3e914bb6de751fbecc0a65622cdc
Meggazord <k_megahed@hotmail.com>
2023-12-17 17:27:19 +0200

    fixed

--- main.c
+++ main.c
@@ -1,10 +1,19 @@
 #include "monty.h"
 
-void process_file(FILE *file) {
+void process_file(FILE *file)
+{
     char *line = NULL;
     size_t len = 0;
     unsigned int line_number = 0;
     stack_t *stack = NULL;
+
+    FILE *file = fopen(argv[1], "r");
+
+    if (!file)
+    {
+        fprintf(stderr, "Error: Can't open file %s\n", argv[1]);
+        exit(EXIT_FAILURE);
+    }
 
     while (getline(&line, &len, file) != -1)
     {
@@ -12,23 +21,30 @@ void process_file(FILE *file) {
 
         char *opcode, *argument;
         opcode = strtok(line, " \n\t");
-        if (opcode == NULL || opcode[0] == '#') {
+        if (opcode == NULL || opcode[0] == '#')
             continue;
-        }
 
         argument = strtok(NULL, " \n\t");
 
-        if (strcmp(opcode, "push") == 0) {
-            if (argument == NULL || !isdigit(*argument)) {
+        if (strcmp(opcode, "push") == 0)
+        {
+            if (argument == NULL || !isdigit(*argument))
+            {
                 fprintf(stderr, "L%d: usage: push integer\n", line_number);
                 free(line);
                 exit(EXIT_FAILURE);
             }
             int value = atoi(argument);
             push(&stack, value);
-        } else if (strcmp(opcode, "pall") == 0) {
+        }
+        
+        else if (strcmp(opcode, "pall") == 0)
+        {
             pall(&stack, line_number);
-        } else {
+        }
+
+        else
+        {
             fprintf(stderr, "L%d: unknown instruction %s\n", line_number, opcode);
             free(line);
             exit(EXIT_FAILURE);
@@ -36,6 +52,7 @@ void process_file(FILE *file) {
     }
 
     free(line);
+    free_stack(stack);
 }
 
 /**
@@ -55,7 +72,9 @@ int main(int argc, char *argv[])
     }
 
     FILE *file = fopen(argv[1], "r");
-    if (file == NULL) {
+
+    if (file == NULL)
+    {
         fprintf(stderr, "Error: Can't open file %s\n", argv[1]);
         return (EXIT_FAILURE);
     }
@@ -64,4 +83,19 @@ int main(int argc, char *argv[])
 
     fclose(file);
     return (0);
+}
+
+void free_stack(stack_t *head)
+{
+    stack_t *tmp;
+
+    if(!head)
+        return;
+
+    while(head != NULL)
+    {
+        tmp = head;
+        head = head->next;
+        free(tmp);
+    }
 }
